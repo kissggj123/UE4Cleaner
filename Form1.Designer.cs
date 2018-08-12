9ab739ce46091dba7ca828b15a90eff4b5943df4
John Ben <dmj1179@hotmail.com>
2018-08-12 21:16:57 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace UE4Cleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.clean = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cachelocation = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "当前操作用户";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cachelocation);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(241, 165);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "信息";
+            // 
+            // clean
+            // 
+            this.clean.BackColor = System.Drawing.Color.MintCream;
+            this.clean.ForeColor = System.Drawing.Color.Orange;
+            this.clean.Location = new System.Drawing.Point(268, 22);
+            this.clean.Name = "clean";
+            this.clean.Size = new System.Drawing.Size(107, 38);
+            this.clean.TabIndex = 9;
+            this.clean.Text = "开始清理";
+            this.clean.UseVisualStyleBackColor = false;
+            this.clean.Click += new System.EventHandler(this.clean_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoEllipsis = true;
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(20, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "准备就绪";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(17, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "工具状态";
+            // 
+            // cachelocation
+            // 
+            this.cachelocation.AutoEllipsis = true;
+            this.cachelocation.AutoSize = true;
+            this.cachelocation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cachelocation.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cachelocation.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.cachelocation.Location = new System.Drawing.Point(20, 43);
+            this.cachelocation.Name = "cachelocation";
+            this.cachelocation.Size = new System.Drawing.Size(58, 19);
+            this.cachelocation.TabIndex = 2;
+            this.cachelocation.Text = "当前缓存";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.ForeColor = System.Drawing.Color.Orange;
+            this.button2.Location = new System.Drawing.Point(268, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "更新日志";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MintCream;
+            this.button4.ForeColor = System.Drawing.Color.Orange;
+            this.button4.Location = new System.Drawing.Point(268, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 38);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "关于程序";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(391, 189);
+            this.Controls.Add(this.clean);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UE4缓存清理 v1.4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label cachelocation;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clean;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace UE4Cleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string []subkeyNames;
+            string ue4path = "当前登陆用户："+ System.Environment.UserName +"\r\n当前登陆电脑：" +Environment.MachineName;
+            cachelocation.Text = ue4path;
+            /*RegistryKey key = Registry.LocalMachine;
+            RegistryKey ue4key = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "");
+            string ue4version = "SOFTWARE\\EpicGames\\Unreal Engine";
+            RegistryKey UE4KEY = ue4key.OpenSubKey(ue4version);
+            String[] keyNameArray = UE4KEY.GetValueNames();
+            foreach (String keyName in keyNameArray)
+            {
+                string keyValue = (string)UE4KEY.GetValue(keyName);
+            }*/
+            //注释代码功能作废
+            if (!System.IO.Directory.Exists(ue4path))
+            {
+                //System.IO.Directory.CreateDirectory(ue4path);//不存在就锁定功能
+                clean.Enabled = false;
+                label3.Text = "不存在缓存 无法清理";
+            }
+            else
+            {
+                clean.Enabled = true;
+                label3.Text = "已检测到缓存 工具准备就绪";
+            }
+
+        }
+
+        private void clean_Click(object sender, EventArgs e)
+        {
+            string ue4path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\UnrealEngine\\Common\\DerivedDataCache";
+            label3.Text = "开始清理缓存";
+            File.SetAttributes(ue4path, FileAttributes.Normal);
+            //System.Diagnostics.Process.Start("del /f " + ue4path);
+            Directory.Delete(ue4path, true);
+            if (!System.IO.Directory.Exists(ue4path))
+            {
+                MessageBox.Show("清理完成 很干净","已完成操作");
+                clean.Enabled = false;
+                label3.Text = "清理成功";
+            }
+            else
+            {
+                MessageBox.Show("出现清理错误", "未完成错误的操作");
+                clean.Enabled = true;
+                label3.Text = "清理出错 检查是否有360等安全保护";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发@北斗家老司机", "Compare on UDK吧");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("2018年8月12日\n\r1.移除缓存目录显示\r\n2.调整了界面\r\n3.修复了一点点的性能问题", "v1.4更新日志");
+        }
+    }
+}
+
+            
+         
